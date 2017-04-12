0e4805e28642700bf48b81972c32ecac9d5a4694
Fernanda <fernandamora88@gmail.com>
2017-04-11 19:30:17 -0700

    initial commit

---
+++ lib/dynamic_array.rb
@@ -0,0 +1,63 @@
+require_relative "static_array"
+
+class DynamicArray
+  attr_reader :length
+
+  def initialize
+    @length = 0
+    @capacity = 8
+    @store = StaticArray.new(@capacity)
+  end
+
+  # O(1)
+  def [](index)
+    index >= @length ? raise("index out of bounds") : @store[index]
+  end
+
+  # O(1)
+  def []=(index, value)
+    resize! if index >= @capacity
+
+    @store[index] = value
+    @length += 1
+  end
+
+  # O(1)
+  def pop
+    
+  end
+
+  # O(1) ammortized; O(n) worst case. Variable because of the possible
+  # resize.
+  def push(val)
+  end
+
+  # O(n): has to shift over all the elements.
+  def shift
+  end
+
+  # O(n): has to shift over all the elements.
+  def unshift(val)
+  end
+
+  protected
+
+  attr_accessor :capacity, :store
+  attr_writer :length
+
+  def check_index(index)
+  end
+
+  # O(n): has to copy over all the elements to the new store.
+  def resize!
+    @capacity *= 2
+    new_array = StaticArray.new(@capacity)
+    i = 0
+    while i < @capacity/2
+      new_array[i] = @store[i]
+      i += 1
+    end
+
+    @store = new_array
+  end
+end
